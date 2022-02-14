b3487a3e4a6d9e6ff6d11ee211c9f714661d1179
Matty O <matt6ong6@gmail.com>
2022-02-14 15:58:49 +0800

    Formatter

--- test/redux_test.exs
+++ test/redux_test.exs
@@ -6,8 +6,10 @@ defmodule ReduxTest do
       case action.type do
         :add ->
           state + action.value
+
         :set ->
           action.value
+
         _ ->
           state
       end
@@ -17,28 +19,25 @@ defmodule ReduxTest do
     {:ok, gen_store} = UsingGenServer.create_store(0, reducer)
     {:ok, process_store} = UsingProcess.create_store(0, reducer)
 
-    {:ok,
-     agent_store: agent_store,
-     gen_store: gen_store,
-     process_store: process_store}
+    {:ok, agent_store: agent_store, gen_store: gen_store, process_store: process_store}
   end
 
   # UsingProcess
 
   test "process dispatch updates the state", %{process_store: process_store} do
-    UsingProcess.dispatch(process_store, %{ type: :add, value: 5 })
+    UsingProcess.dispatch(process_store, %{type: :add, value: 5})
     assert UsingProcess.get_state(process_store) == 5
   end
 
   test "process subscribers gets notified and unsubscribed",
-    %{process_store: process_store} do
-
-    unsubscribe = UsingProcess.subscribe(process_store, fn store ->
-      IO.puts "The state is #{UsingProcess.get_state(store)}"
-    end)
+       %{process_store: process_store} do
+    unsubscribe =
+      UsingProcess.subscribe(process_store, fn store ->
+        IO.puts("The state is #{UsingProcess.get_state(store)}")
+      end)
 
     f = fn ->
-      UsingProcess.dispatch(process_store, %{ type: :add, value: 10 })
+      UsingProcess.dispatch(process_store, %{type: :add, value: 10})
     end
 
     assert ExUnit.CaptureIO.capture_io(f) == "The state is 10\n"
@@ -57,19 +56,19 @@ defmodule ReduxTest do
   # UsingGenServer
 
   test "gen dispatch updates the state", %{gen_store: gen_store} do
-    UsingGenServer.dispatch(gen_store, %{ type: :add, value: 5 })
+    UsingGenServer.dispatch(gen_store, %{type: :add, value: 5})
     assert UsingGenServer.get_state(gen_store) == 5
   end
 
   test "gen subscribers gets notified and unsubscribed",
-    %{gen_store: gen_store} do
-
-    unsubscribe = UsingGenServer.subscribe(gen_store, fn store ->
-      IO.puts "The state is #{UsingGenServer.get_state store}"
-    end)
+       %{gen_store: gen_store} do
+    unsubscribe =
+      UsingGenServer.subscribe(gen_store, fn store ->
+        IO.puts("The state is #{UsingGenServer.get_state(store)}")
+      end)
 
     f = fn ->
-      UsingGenServer.dispatch(gen_store, %{ type: :add, value: 10 })
+      UsingGenServer.dispatch(gen_store, %{type: :add, value: 10})
     end
 
     assert ExUnit.CaptureIO.capture_io(f) == "The state is 10\n"
@@ -86,19 +85,19 @@ defmodule ReduxTest do
   # UsingAgent
 
   test "agent dispatch updates the state", %{agent_store: agent_store} do
-    UsingAgent.dispatch(agent_store, %{ type: :add, value: 5 })
+    UsingAgent.dispatch(agent_store, %{type: :add, value: 5})
     assert UsingAgent.get_state(agent_store) == 5
   end
 
   test "agent subscribers gets notified and unsubscribed",
-    %{agent_store: agent_store} do
-
-    unsubscribe = UsingAgent.subscribe(agent_store, fn store ->
-      IO.puts "The state is #{UsingAgent.get_state store}"
-    end)
+       %{agent_store: agent_store} do
+    unsubscribe =
+      UsingAgent.subscribe(agent_store, fn store ->
+        IO.puts("The state is #{UsingAgent.get_state(store)}")
+      end)
 
     f = fn ->
-      UsingAgent.dispatch(agent_store, %{ type: :add, value: 10 })
+      UsingAgent.dispatch(agent_store, %{type: :add, value: 10})
     end
 
     assert ExUnit.CaptureIO.capture_io(f) == "The state is 10\n"
@@ -107,7 +106,7 @@ defmodule ReduxTest do
     unsubscribe.()
     assert ExUnit.CaptureIO.capture_io(f) == ""
 
-    store = Agent.get agent_store, & &1
+    store = Agent.get(agent_store, & &1)
     assert store.state == 20
     assert store.subscribers == []
   end
